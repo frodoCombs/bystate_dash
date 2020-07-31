55628c81be5ec0137ebbf2bb0e19eac02d1b11eb
Ford Combs <fcombs90@gmail.com>
2020-07-31 13:31:03 -0400

    select-all-button added

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+
 #url = 'https://raw.githubusercontent.com/frodoCombs/covid_dash/master/covid.csv'
 cases = pd.read_csv('covid.csv',sep=",")
 
@@ -17,31 +18,48 @@ by_state = by_state.groupby(by_state['State'],as_index=False).sum()
 states = by_state.State.unique()
 states.sort()
 	
+#button color
+wh = {'font-size':'18px','background-color':'white','height':'40px','width': '110px','border-width':'1px','border-style':'solid','border-radius':'2px',
+'border-color':'Gainsboro','font-weight':'bold','margin-left':'30px'}
+rd = {'font-size':'18px','background-color': 'blue','height':'40px','width': '110px','border-width':'1px','border-radius':'2px','border-style':'solid',
+'border-color':'Gainsboro','font-weight':'bold','color':'white','margin-left':'30px'}
+
+
 app = dash.Dash(__name__)
 server = app.server
 
-app.layout = html.Div([
-	html.H3("Choose which states to visualize"),
-	html.Div([dcc.Checklist(id='state-select', options=[{'label': i, 'value': i} for i in states],
-						   value=states, style={'width': '75%'})]),
-	html.H3("Choose the date range"),
-    dcc.DatePickerRange(
-        id='my-date-picker-range',
-        min_date_allowed=dt(2020, 1, 22),
-        max_date_allowed=dt(2020, 7, 27),
-        initial_visible_month=dt(2020, 2, 1),
-		start_date=dt(2020,1,22).date(),
-        end_date=dt(2020,7,27).date()
-    ),
-	dcc.Graph('lines', config={'displayModeBar': False})])
+app.layout = html.Div(children = [
+	html.H1("Confirmed Covid-19 Cases - by state"),
+	html.H4("Choose which states to visualize"),
+	html.Div([dcc.Checklist(id='state-select', 
+			options=[{'label': i, 'value': i} for i in states],
+			value=['NY','TX'], labelStyle={'display':'inline-block'},
+			style={'width':'60%','margin':'auto'})]),
+	html.Button('All States',id='button',n_clicks=0,style=wh),
+	html.H4("Choose the date range"),
+	dcc.DatePickerRange(
+		id='my-date-picker-range',
+		min_date_allowed=dt(2020, 1, 22), max_date_allowed=dt(2020, 7, 30),
+		initial_visible_month=dt(2020, 2, 1), start_date=dt(2020,1,22).date(),
+		end_date=dt(2020,7,29).date(),style={'margin-left':'30px'}),
+	dcc.Graph('lines', config={'displayModeBar': False}),
+	html.Hr(),
+	html.P(["This interactive graph was created by ",dcc.Link('Ford Combs',href="https://medium.com/@fordcombs",refresh=True), " using the ",dcc.Link('Dash framework.',href="https://dash.plotly.com/introduction",refresh=True), " The data can be downloaded ",dcc.Link('here',href="https://usafacts.org/visualizations/coronavirus-covid-19-spread-map/",refresh=True),'.'])
+	])
 
 @app.callback(
-	Output('lines', 'figure'),
+	[Output('lines', 'figure'),Output('button', 'style')],
 	[Input('state-select', 'value'),
+	Input('button','n_clicks'),
 	Input('my-date-picker-range', 'start_date'),
-	Input('my-date-picker-range', 'end_date')]
+	Input('my-date-picker-range', 'end_date'),]
 )
-def update_graph(grpnames,start_date,end_date):
+def update_graph(grpnames,clicks,start_date,end_date):
+	if clicks%2 > 0:
+		style = rd
+	else:
+		style = wh
+		
 	# format date to use as range
 	start_date = start_date.split('-')
 	end_date = end_date.split('-')
@@ -51,19 +69,26 @@ def update_graph(grpnames,start_date,end_date):
 	end = dt.strptime(end_date, "%m/%d/%y")
 	date_range = [start + timedelta(days=x) for x in range(0, (end-start).days)]
 	date_range = [d.strftime("%-m/%-d/%y")for d in date_range]
-
+	
 	# Plot the data
 	df = by_state.copy()
 	df = pd.melt(df,id_vars='State')
-	df = df[df['State'].isin(grpnames)]
+	if style == rd:
+		pass
+	else:
+		df = df[df['State'].isin(grpnames)]
 	df = df[df['variable'].isin(date_range)]
 
 	
 	import plotly.express as px
-	return px.line(
+	plot = px.line(
 	data_frame=df,x='variable',y='value',color='State',
 	labels ={'variable':'Date','value':'# of Confirmed Cases'}
 	)
+
+
+	return plot, style
+	
 
 if __name__ == '__main__':
     app.run_server(debug=False)
